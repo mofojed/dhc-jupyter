9ff53ebd2df09f347cab7555f127af2e42c16096
Mike Bender <mikebender@deephaven.io>
2022-07-12 10:37:39 -0400

    First commit
    It's not working ... no dh-core script imported, so could do that garbage where we copy the compiled JS API here ... or ... something else.

---
+++ README.md
@@ -0,0 +1,88 @@
+# dhc-jupyter
+
+[![Build Status](https://travis-ci.org/deephaven/dhc-jupyter.svg?branch=master)](https://travis-ci.org/deephaven/dhc_jupyter)
+[![codecov](https://codecov.io/gh/deephaven/dhc-jupyter/branch/master/graph/badge.svg)](https://codecov.io/gh/deephaven/dhc-jupyter)
+
+A Custom Jupyter Widget Library
+
+## WIP Building/Running
+
+Source the python environment:
+
+```bash
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/openjdk-11.jdk/Contents/Home
+python3 -m venv .venv
+source .venv/bin/activate
+pip install --upgrade pip setuptools
+pip install deephaven_server jupyter
+```
+
+## Installation
+
+You can install using `pip`:
+
+```bash
+pip install dhc_jupyter
+```
+
+If you are using Jupyter Notebook 5.2 or earlier, you may also need to enable
+the nbextension:
+
+```bash
+jupyter nbextension enable --py [--sys-prefix|--user|--system] dhc_jupyter
+```
+
+## Development Installation
+
+Create a dev environment:
+
+```bash
+conda create -n dhc_jupyter-dev -c conda-forge nodejs yarn python jupyterlab
+conda activate dhc_jupyter-dev
+```
+
+Install the python. This will also build the TS package.
+
+```bash
+pip install -e ".[test, examples]"
+```
+
+When developing your extensions, you need to manually enable your extensions with the
+notebook / lab frontend. For lab, this is done by the command:
+
+```
+jupyter labextension develop --overwrite .
+yarn run build
+```
+
+For classic notebook, you need to run:
+
+```
+jupyter nbextension install --sys-prefix --symlink --overwrite --py dhc_jupyter
+jupyter nbextension enable --sys-prefix --py dhc_jupyter
+```
+
+Note that the `--symlink` flag doesn't work on Windows, so you will here have to run
+the `install` command every time that you rebuild your extension. For certain installations
+you might also need another flag instead of `--sys-prefix`, but we won't cover the meaning
+of those flags here.
+
+### How to see your changes
+
+#### Typescript:
+
+If you use JupyterLab to develop then you can watch the source directory and run JupyterLab at the same time in different
+terminals to watch for changes in the extension's source and automatically rebuild the widget.
+
+```bash
+# Watch the source directory in one terminal, automatically rebuilding when needed
+yarn run watch
+# Run JupyterLab in another terminal
+jupyter lab
+```
+
+After a change wait for the build to finish and then refresh your browser and the changes should take effect.
+
+#### Python:
+
+If you make a change to the python code then you will need to restart the notebook kernel to have it take effect.
---
+++ src/widget.ts
@@ -0,0 +1,107 @@
+// Copyright (c) Mike Bender
+// Distributed under the terms of the Modified BSD License.
+
+import {
+  DOMWidgetModel,
+  DOMWidgetView,
+  ISerializers,
+} from '@jupyter-widgets/base';
+import { v4 as uuidv4 } from 'uuid';
+import dh from '@deephaven/jsapi-shim';
+import Log from '@deephaven/log';
+import { MODULE_NAME, MODULE_VERSION } from './version';
+
+const log = Log.module('dhc-jupyter.widget');
+
+// Import the CSS
+import '../css/widget.css';
+
+export class ExampleModel extends DOMWidgetModel {
+  defaults() {
+    return {
+      ...super.defaults(),
+      _model_name: ExampleModel.model_name,
+      _model_module: ExampleModel.model_module,
+      _model_module_version: ExampleModel.model_module_version,
+      _view_name: ExampleModel.view_name,
+      _view_module: ExampleModel.view_module,
+      _view_module_version: ExampleModel.view_module_version,
+      value: 'Hello World',
+      port: -1,
+      table_id: 'UNKNOWN_ID',
+    };
+  }
+
+  static serializers: ISerializers = {
+    ...DOMWidgetModel.serializers,
+    // Add any extra serializers here
+  };
+
+  static model_name = 'ExampleModel';
+  static model_module = MODULE_NAME;
+  static model_module_version = MODULE_VERSION;
+  static view_name = 'ExampleView'; // Set to null if no view
+  static view_module = MODULE_NAME; // Set to null if no view
+  static view_module_version = MODULE_VERSION;
+}
+
+export class ExampleView extends DOMWidgetView {
+  private iframe: HTMLIFrameElement;
+
+  render() {
+    if (!this.iframe) {
+      this.iframe = document.createElement('iframe');
+    }
+    this.el.appendChild(this.iframe);
+
+    // this.el.classList.add('custom-widget');
+
+    this.init_element();
+    // this.model.on('change:value', this.update_element, this);
+    // this.model.on('change:my_globals', this.update_element, this);
+    // this.model.on('change:model_id', this.update_element, this);
+  }
+
+  async init_element() {
+    const serverUrl = this.model.get('server_url');
+    const widgetId = this.model.get('widget_id');
+    log.info('init_element for widget', widgetId, serverUrl);
+
+    // We need to generate an ID, connect to a session, and assign the table to a new variable
+    const tableId = `widget_t_${uuidv4().replace(/-/g, '_')}`;
+
+    log.info('tableId', tableId);
+
+    const connection = new dh.IdeConnection(serverUrl);
+
+    log.info('connection', connection);
+
+    // await connection.getConsoleTypes();
+
+    const ide = await connection.startSession('python');
+
+    log.info('ide', ide);
+
+    const code = `
+from ipywidgets import Widget
+${tableId} = Widget.widgets['${widgetId}'].table
+    `;
+
+    log.info('running code', code);
+
+    const result = await (ide as any).runCode(code);
+    // connection = new dh.IdeConnection(window.location.protocol + "//" + window.location.host);
+
+    const iframeUrl = `${serverUrl}/iframe/table/?name=${tableId}`;
+
+    log.info('code ran! opening table in iframe...', result, iframeUrl);
+
+    this.iframe.src = iframeUrl;
+
+    // this.iframe.src = this.model.get('value');
+    // this.el.textContent = `model_id--->${this.model.get('model_id')}`;
+    // this.el.textContent = `text--->${this.model.get('value')},${this.model.get(
+    //   'table_id'
+    // )},${this.model.get('port')}`;
+  }
+}
