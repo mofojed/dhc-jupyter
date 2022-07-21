edc05f9cb00c2dd789ca260a055d82706d63373a
Mike Bender <mikebender@deephaven.io>
2022-07-21 10:33:51 -0400

    Clean up the roughness
    - Put transparent styling into widget.css so it doesn't need to be included as part of notebook
    - Get the main modules global dict using __main__.__dict__ instead of needing to pass it in
    - Get the server port directly rather than having to pass it in. No more need for the DeephavenWidget.init to be called!

--- dhc_jupyter/deephaven.py
+++ dhc_jupyter/deephaven.py
@@ -1,65 +1,73 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# Copyright (c) Mike Bender.
-# Distributed under the terms of the Modified BSD License.
+#
+# Copyright (c) 2016-2022 Deephaven Data Labs and Patent Pending
+#
 
 """
-TODO: Add module docstring
+Module for displaying Deephaven widgets within interactive python environments.
 """
 
-from ipywidgets import DOMWidget, Widget
+import __main__
+from ipywidgets import DOMWidget
 from traitlets import Unicode, Integer
 from deephaven_server import Server
 from uuid import uuid4
 from ._frontend import module_name, module_version
 
-DEFAULT_WIDTH = 900
-DEFAULT_HEIGHT = 600
-DEFAULT_SERVER_URL = "http://localhost:8080"
+def _str_object_type(obj):
+  """Returns the object type as a string value"""
+  return f"{obj.__class__.__module__}.{obj.__class__.__name__}"
+
+def _path_for_object(obj):
+  """Return the iframe path for the specified object. Inspects the class name to determine."""
+  name = _str_object_type(obj)
+  if name == 'deephaven.table.Table':
+    return 'table'
+  # TODO: Add more types (after embedded Figure support in iframes)
+  raise TypeError("Unknown object type")
+
 
 class DeephavenWidget(DOMWidget):
-    _globals = globals()
-    _default_width = DEFAULT_WIDTH
-    _default_height = DEFAULT_HEIGHT
-    _default_server_url = DEFAULT_SERVER_URL
-
-    # Need to call the initialize method to correctly set the globals var
-    @staticmethod
-    def init(globals, width=DEFAULT_WIDTH, height=DEFAULT_HEIGHT, url=DEFAULT_SERVER_URL):
-      DeephavenWidget._globals = globals
-      DeephavenWidget._default_server_url = url
-      DeephavenWidget._default_width = width
-      DeephavenWidget._default_height = height
-
     """A wrapper for viewing DeephavenWidgets in IPython
     """
-    _model_name = Unicode('ExampleModel').tag(sync=True)
+    _model_name = Unicode('DeephavenModel').tag(sync=True)
     _model_module = Unicode(module_name).tag(sync=True)
     _model_module_version = Unicode(module_version).tag(sync=True)
-    _view_name = Unicode('ExampleView').tag(sync=True)
+    _view_name = Unicode('DeephavenView').tag(sync=True)
     _view_module = Unicode(module_name).tag(sync=True)
     _view_module_version = Unicode(module_version).tag(sync=True)
 
-    widget_id = Unicode().tag(sync=True)
-    table_id = Unicode().tag(sync=True)
+    object_id = Unicode().tag(sync=True)
+    object_type = Unicode().tag(sync=True)
     server_url = Unicode().tag(sync=True)
+    iframe_url = Unicode().tag(sync=True)
     width = Integer().tag(sync=True)
     height = Integer().tag(sync=True)
 
+    def __init__(self, deephaven_object, height=600, width=0):
+        """Create a Deephaven widget for displaying in an interactive Python console.
 
-    # We have to pass in the main `globals` so that we can assign our temporary table variable correctly
-    def __init__(self, table, width=_default_width, height=_default_height, url=_default_server_url):
+        Args:
+            deephaven_object (Table): the Deephaven object to display
+            height (int): the height of the table
+            width (int): the width of the table. Set to 0 to take up full width of notebook.
+        """
         super(DeephavenWidget, self).__init__()
 
         # Generate a new table ID using a UUID prepended with a `t_` prefix
-        table_id = f"t_{str(uuid4()).replace('-', '_')}"
+        object_id = f"t_{str(uuid4()).replace('-', '_')}"
 
-        # Add the table to the globals list so it can be retrieved by the iframe
-        DeephavenWidget._globals[table_id] = table
+        # Generate the iframe_url from the object type
+        server_url = f"http://localhost:{Server.instance.port}"
+        iframe_url = f"{server_url}/iframe/{_path_for_object(deephaven_object)}/?name={object_id}"
 
-        self.set_trait('widget_id', self.model_id)
-        self.set_trait('server_url', url)
-        self.set_trait('table_id', table_id)
+        # Add the table to the main modules globals list so it can be retrieved by the iframe
+        __main__.__dict__[object_id] = deephaven_object
+
+        self.set_trait('server_url', server_url)
+        self.set_trait('iframe_url', iframe_url)
+        self.set_trait('object_id', object_id)
+        self.set_trait('object_type', _str_object_type(deephaven_object))
         self.set_trait('width', width)
         self.set_trait('height', height)
--- src/widget.ts
+++ src/widget.ts
@@ -14,19 +14,16 @@ const log = Log.module('dhc-jupyter.widget');
 // Import the CSS
 import '../css/widget.css';
 
-export class ExampleModel extends DOMWidgetModel {
+export class DeephavenModel extends DOMWidgetModel {
   defaults() {
     return {
       ...super.defaults(),
-      _model_name: ExampleModel.model_name,
-      _model_module: ExampleModel.model_module,
-      _model_module_version: ExampleModel.model_module_version,
-      _view_name: ExampleModel.view_name,
-      _view_module: ExampleModel.view_module,
-      _view_module_version: ExampleModel.view_module_version,
-      value: 'Hello World',
-      port: -1,
-      table_id: 'UNKNOWN_ID',
+      _model_name: DeephavenModel.model_name,
+      _model_module: DeephavenModel.model_module,
+      _model_module_version: DeephavenModel.model_module_version,
+      _view_name: DeephavenModel.view_name,
+      _view_module: DeephavenModel.view_module,
+      _view_module_version: DeephavenModel.view_module_version,
     };
   }
 
@@ -35,31 +32,33 @@ export class ExampleModel extends DOMWidgetModel {
     // Add any extra serializers here
   };
 
-  static model_name = 'ExampleModel';
+  static model_name = 'DeephavenModel';
   static model_module = MODULE_NAME;
   static model_module_version = MODULE_VERSION;
-  static view_name = 'ExampleView'; // Set to null if no view
+  static view_name = 'DeephavenView'; // Set to null if no view
   static view_module = MODULE_NAME; // Set to null if no view
   static view_module_version = MODULE_VERSION;
 }
 
-export class ExampleView extends DOMWidgetView {
+export class DeephavenView extends DOMWidgetView {
   private iframe: HTMLIFrameElement;
 
   render() {
-    const serverUrl = this.model.get('server_url');
-    const tableId = this.model.get('table_id');
-    const iframeUrl = `${serverUrl}/iframe/table/?name=${tableId}`;
+    const iframeUrl = this.model.get('iframe_url');
     const width = this.model.get('width');
     const height = this.model.get('height');
-    log.info('init_element for widget', tableId, serverUrl, iframeUrl);
+    log.info('init_element for widget', iframeUrl, width, height);
 
     this.iframe = document.createElement('iframe');
     this.iframe.src = iframeUrl;
-    this.iframe.style.width = `${width}px`;
-    this.iframe.style.height = `${height}px`;
-    this.iframe.width = width;
-    this.iframe.height = height;
+    if (width > 0) {
+      this.iframe.width = width;
+      this.iframe.style.width = `${width}px`;
+    }
+    if (height > 0) {
+      this.iframe.height = height;
+      this.iframe.style.height = `${height}px`;
+    }
     this.el.className = 'dhc-jupyter-widget';
     this.el.appendChild(this.iframe);
   }
